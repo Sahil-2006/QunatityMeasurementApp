042a1ef7b46f8188c367db3d9e11d37452a87d2e
Sahil Shabbirali Haryani <137910815+Sahil-2006@users.noreply.github.com>
2026-05-05 13:23:09 +0300

    Sahil fixed UC1

--- QuantityMeasurementApp.java
+++ QuantityMeasurementApp.java
@@ -1,20 +1,57 @@
 package com.apps.quantitymeasurement;
 
+/**
+ * QuantityMeasurementApp - UC1: Feet measurement equality
+ * 
+ * This class is responsible for checking the equality of two numerical values
+ * measured in feet in the Quantity Measurement Application.
+ */
 public class QuantityMeasurementApp {
 
-    public static <U extends IMeasurable> boolean demonstrateEquality(Quantity<U> q1, Quantity<U> q2) {
-        return q1.equals(q2);
+    // Inner class to represent Feet measurement
+    public static class Feet {
+        private final double value;
+
+        public Feet(double value) {
+            this.value = value;
+        }
+
+        /**
+         * Override equals() method to compare two Feet objects based on their value
+         * 
+         * Important Checks:
+         * 1. Reference Check: If both references point to the same object, return true
+         * 2. Null Check: If the compared object is null, return false
+         * 3. Type Check: If the compared object is not of type Feet, return false
+         * 4. Value Comparison: Use Double.compare() to compare the double values for equality
+         * 
+         * @param obj The object to compare with
+         * @return true if both Feet objects have the same value, false otherwise
+         */
+        @Override
+        public boolean equals(Object obj) {
+            // 1. Reference Check
+            if (this == obj) {
+                return true;
+            }
+
+            // 2. Null Check and 3. Type Check
+            if (obj == null || getClass() != obj.getClass()) {
+                return false;
+            }
+
+            // 4. Value Comparison
+            Feet feet = (Feet) obj;
+            return Double.compare(feet.value, value) == 0;
+        }
     }
 
+    // Main method to demonstrate Feet equality check
     public static void main(String[] args) {
-        // Length Example
-        Quantity<LengthUnit> oneFoot = new Quantity<>(1.0, LengthUnit.FEET);
-        Quantity<LengthUnit> twelveInches = new Quantity<>(12.0, LengthUnit.INCHES);
-        System.out.println("1 ft == 12 in? " + demonstrateEquality(oneFoot, twelveInches));
-
-        // Weight Example
-        Quantity<WeightUnit> oneKg = new Quantity<>(1.0, WeightUnit.KILOGRAM);
-        Quantity<WeightUnit> thousandGrams = new Quantity<>(1000.0, WeightUnit.GRAM);
-        System.out.println("1 kg == 1000 g? " + demonstrateEquality(oneKg, thousandGrams));
+        Feet feet1 = new Feet(1.0);
+        Feet feet2 = new Feet(1.0);
+        
+        System.out.println("Input: 1.0 ft and 1.0 ft");
+        System.out.println("Output: Equal (" + feet1.equals(feet2) + ")");
     }
 }
