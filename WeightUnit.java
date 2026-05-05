02a5c16b9651921f41bec7596718a4fda801a53f
Sahil Shabbirali Haryani <137910815+Sahil-2006@users.noreply.github.com>
2026-05-05 13:16:22 +0300

    Sahil Added WeightUnit to UC10

---
+++ WeightUnit.java
@@ -0,0 +1,24 @@
+package com.apps.quantitymeasurement;
+
+public enum WeightUnit implements IMeasurable {
+    GRAM(1.0), KILOGRAM(1000.0), POUND(453.592), TONNE(1000000.0);
+
+    private final double conversionFactor;
+
+    WeightUnit(double conversionFactor) {
+        this.conversionFactor = conversionFactor;
+    }
+
+    @Override
+    public double getConversionFactor() { return conversionFactor; }
+
+    @Override
+    public double convertToBaseUnit(double value) {
+        return Math.round((value * conversionFactor) * 100.0) / 100.0;
+    }
+
+    @Override
+    public double convertFromBaseUnit(double baseValue) {
+        return Math.round((baseValue / conversionFactor) * 100.0) / 100.0;
+    }
+}
