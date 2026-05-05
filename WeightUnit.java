2b0a1889d88db5f84cca071c5253441ef00ae701
Sahil Shabbirali Haryani <137910815+Sahil-2006@users.noreply.github.com>
2026-05-05 13:47:34 +0300

    Update WeightUnit.java

--- WeightUnit.java
+++ WeightUnit.java
@@ -1,24 +1,22 @@
 package com.apps.quantitymeasurement;
 
 public enum WeightUnit implements IMeasurable {
-    GRAM(1.0), KILOGRAM(1000.0), POUND(453.592), TONNE(1000000.0); //
+    GRAM(1.0), KILOGRAM(1000.0), POUND(453.592), TONNE(1000000.0);
 
     private final double conversionFactor;
 
-    WeightUnit(double conversionFactor) {
-        this.conversionFactor = conversionFactor;
-    }
+    WeightUnit(double conversionFactor) { this.conversionFactor = conversionFactor; }
 
     @Override
     public double getConversionFactor() { return conversionFactor; }
 
     @Override
     public double convertToBaseUnit(double value) {
-        return Math.round((value * conversionFactor) * 100.0) / 100.0; //
+        return Math.round((value * conversionFactor) * 100.0) / 100.0;
     }
 
     @Override
     public double convertFromBaseUnit(double baseValue) {
-        return Math.round((baseValue / conversionFactor) * 100.0) / 100.0; //
+        return Math.round((baseValue / conversionFactor) * 100.0) / 100.0;
     }
 }
