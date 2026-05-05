422a74e9f014cfa27b5861c0ec6d639b6f8848bc
Sahil Shabbirali Haryani <137910815+Sahil-2006@users.noreply.github.com>
2026-05-05 13:46:45 +0300

    Update QuantityMeasurementApp.java

--- QuantityMeasurementApp.java
+++ QuantityMeasurementApp.java
@@ -1,25 +1,15 @@
 package com.apps.quantitymeasurement;
 
 public class QuantityMeasurementApp {
-
-    public static <U extends IMeasurable> boolean demonstrateEquality(Quantity<U> q1, Quantity<U> q2) {
-        return q1.equals(q2); //
-    }
-
     public static void main(String[] args) {
-        // UC11: Volume Example
-        Quantity<VolumeUnit> oneLitre = new Quantity<>(1.0, VolumeUnit.LITRE);
-        Quantity<VolumeUnit> thousandMl = new Quantity<>(1000.0, VolumeUnit.MILLILITRE);
-        System.out.println("1 L == 1000 mL? " + demonstrateEquality(oneLitre, thousandMl));
+        // UC11: Volume
+        Quantity<VolumeUnit> litre = new Quantity<>(1.0, VolumeUnit.LITRE);
+        Quantity<VolumeUnit> ml = new Quantity<>(1000.0, VolumeUnit.MILLILITRE);
+        System.out.println("1L == 1000mL? " + litre.equals(ml));
 
-        // UC12: Subtraction Example
-        Quantity<LengthUnit> tenFeet = new Quantity<>(10.0, LengthUnit.FEET);
-        Quantity<LengthUnit> sixInches = new Quantity<>(6.0, LengthUnit.INCHES);
-        System.out.println("10 ft - 6 in (in ft): " + tenFeet.subtract(sixInches, LengthUnit.FEET));
-
-        // UC12: Division Example
-        Quantity<WeightUnit> twoKg = new Quantity<>(2.0, WeightUnit.KILOGRAM);
-        Quantity<WeightUnit> fiveHundredG = new Quantity<>(500.0, WeightUnit.GRAM);
-        System.out.println("2 kg / 500 g: " + twoKg.divide(fiveHundredG)); // Output: 4.0
+        // UC12/13: Centralized Arithmetic
+        Quantity<LengthUnit> feet = new Quantity<>(10.0, LengthUnit.FEET);
+        Quantity<LengthUnit> inches = new Quantity<>(6.0, LengthUnit.INCHES);
+        System.out.println("10ft - 6in in ft: " + feet.subtract(inches, LengthUnit.FEET));
     }
 }
