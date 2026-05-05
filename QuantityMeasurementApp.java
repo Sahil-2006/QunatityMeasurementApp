af8b20f9489ec9ec1425cc36ec2c583c5d8ca36f
Sahil Shabbirali Haryani <137910815+Sahil-2006@users.noreply.github.com>
2026-05-05 10:51:43 +0300

    Sahil Added UC5
    Removed equality demonstration methods and added length conversion methods.

--- QuantityMeasurementApp.java
+++ QuantityMeasurementApp.java
@@ -2,31 +2,27 @@ package com.apps.quantitymeasurement;
 
 public class QuantityMeasurementApp {
 
-    public static boolean demonstrateLengthEquality(Length length1, Length length2) {
-        return length1.equals(length2);
+    /**
+     * Method 1: Takes raw numeric value and two units.
+     */
+    public static Length demonstrateLengthConversion(double value, Length.LengthUnit fromUnit, Length.LengthUnit toUnit) {
+        Length sourceLength = new Length(value, fromUnit);
+        return sourceLength.convertTo(toUnit);
     }
 
-    public static void demonstrateFeetEquality() {
-        Length feet1 = new Length(1.0, Length.LengthUnit.FEET);
-        Length feet2 = new Length(1.0, Length.LengthUnit.FEET);
-        System.out.println("Feet Equality: " + demonstrateLengthEquality(feet1, feet2));
-    }
-
-    public static void demonstrateInchesEquality() {
-        Length inch1 = new Length(1.0, Length.LengthUnit.INCHES);
-        Length inch2 = new Length(1.0, Length.LengthUnit.INCHES);
-        System.out.println("Inches Equality: " + demonstrateLengthEquality(inch1, inch2));
-    }
-
-    public static void demonstrateFeetInchesComparison() {
-        Length feet = new Length(1.0, Length.LengthUnit.FEET);
-        Length inches = new Length(12.0, Length.LengthUnit.INCHES);
-        System.out.println("1 Feet vs 12 Inches Equality: " + demonstrateLengthEquality(feet, inches));
+    /**
+     * Method 2 (Overloaded): Takes an existing object and target unit.
+     */
+    public static Length demonstrateLengthConversion(Length length, Length.LengthUnit toUnit) {
+        return length.convertTo(toUnit);
     }
 
     public static void main(String[] args) {
-        demonstrateFeetEquality();
-        demonstrateInchesEquality();
-        demonstrateFeetInchesComparison();
+        // Example conversions
+        System.out.println("1.0 FEET to INCHES: " + demonstrateLengthConversion(1.0, Length.LengthUnit.FEET, Length.LengthUnit.INCHES));
+        System.out.println("3.0 YARDS to FEET: " + demonstrateLengthConversion(3.0, Length.LengthUnit.YARDS, Length.LengthUnit.FEET));
+        
+        Length hundredCm = new Length(100.0, Length.LengthUnit.CENTIMETERS);
+        System.out.println("100.0 CM to INCHES: " + demonstrateLengthConversion(hundredCm, Length.LengthUnit.INCHES));
     }
 }
